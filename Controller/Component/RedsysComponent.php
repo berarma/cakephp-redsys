22ff1401926bf4184a3818839f1871633d6879ec
Bernat Arlandis <berarma@hotmail.com>
2016-10-31 11:04:52 +0100

    Add Redsys config to provided settings

--- Controller/Component/RedsysComponent.php
+++ Controller/Component/RedsysComponent.php
@@ -30,6 +30,13 @@ class RedsysComponent extends Component {
 
 	public $Controller;
 
+	public function __construct(ComponentCollection $collection, $settings = array()) {
+		if (Configure::check('Redsys')) {
+			$settings += Configure::read('Redsys');
+		}
+		parent::__construct($collection, $settings);
+	}
+
 /**
  * {@inheritDoc}
  */
